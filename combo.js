b3fd2b11c6681ffe4527c99b7183316649c9afb3
Todd Spainhour <toddspainhour@gmail.com>
2020-01-20 21:31:40 -0600

    combo questions, number 1 complete

---
+++ combo.js
@@ -0,0 +1,57 @@
+console.log("Your combo js file is connected.")
+
+// Challenge #1
+// Create a function called evenOdd that takes an integer as an argument and
+// returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+const pickANumber = ""
+
+const evenOdd = (pickANumber) => {
+ if (pickANumber % 2 === 0) {
+     return "The number you picked is even!"
+ } else {
+     return "The number you picked is odd!"
+ }
+}
+
+
+console.log(evenOdd(24623984293))
+
+
+
+
+
+
+
+
+// Challenge #2
+// given a number, find its opposite. 2 = -2
+
+
+
+
+
+
+
+
+
+
+
+// Challenge #3
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+// Console true if the given string is a palindrome. Otherwise, console false.
+
+
+
+
+
+
+
+
+
+
+// Challenge #4
+// Lawrence the wide mouth frog is particularly interested in the eating habits of other creatures.
+// He just can't stop asking the creatures he encounters what they like to eat. But then he meet the alligator who just LOVES to eat the lips of wide-mouthed frogs!
+// Given a string with the animal name, that Lawrence encounters, output small if the animal is an alligator (case insensitive) otherwise return wide.
