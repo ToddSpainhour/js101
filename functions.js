9ef5482b7b7fdb97628f0242f21ca9596dab1f84
Todd Spainhour <toddspainhour@gmail.com>
2020-01-20 19:10:48 -0600

    added two functions

--- functions.js
+++ functions.js
@@ -21,3 +21,25 @@ return `My favorite dog breed is ${dog}`
 
 console.log(dogBreed('lab'))
 console.log(dogBreed('actually I prefer cats.'))
+
+//challenge one - create a function called add42 that take any number and adds 42
+
+
+
+const add42 = (anyNumber) => {
+return anyNumber + 42
+}
+
+console.log(add42(8))
+
+
+
+
+//challenge two - write a function called oldAge that takes the year someone was born and figures out how old they will be in 2099
+
+
+const ageIn2099 = (yearOfBirth) => {
+return 2099 - yearOfBirth;
+}
+
+console.log(ageIn2099(1980));
